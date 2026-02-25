13a4a5036226fa0051de2fd530d87320abcd8ef5
irene <irene.ramos@jakala.com>
2026-02-25 09:02:22 +0100

    Componentes sesión 1 modificados por irene

--- ui.apps/src/main/content/jcr_root/apps/itv/components/supertitle/clientlib/js/supertitle.js
+++ ui.apps/src/main/content/jcr_root/apps/itv/components/supertitle/clientlib/js/supertitle.js
@@ -1,23 +1,28 @@
 (function (document) {
   "use strict";
 
-  function initTitleClick() {
-    var titles = document.querySelectorAll(".js-title");
+  function initScrollReveal() {
+    const revealObject = document.querySelectorAll(".cmp-supertitle--reveal");
 
-    if (!titles || titles.length === 0) {
+    if (revealObject.length === 0) {
       return;
     }
 
-    titles.forEach(function (el) {
-      el.addEventListener("click", function () {
-        alert("hola soy title");
-      });
+    const observer = new IntersectionObserver((entries, obs) => {
+            entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+                obs.unobserve(entry.target);
+              }
+            });
+    },{
+    threshold: 0.1
     });
+    revealObject.forEach(el => observer.observe(el));
   }
-  // Ejecutar cuando el DOM esté listo
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", initTitleClick);
-  } else {
-    initTitleClick();
-  }
+    if (document.readyState === "loading") {
+      document.addEventListener("DOMContentLoaded", initScrollReveal);
+    } else {
+      initScrollReveal();
+    }
 })(document);
