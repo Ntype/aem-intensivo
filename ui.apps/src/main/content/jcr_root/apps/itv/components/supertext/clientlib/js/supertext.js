295c7cd87b4560bbb3eb0fb9a11692f36ec91f8d
Jorge García Estévez <jorge.garciaestevez@jakala.com>
2026-02-25 08:28:40 +0100

    supertext: al hacer click cambia colorRojo -- superimage: mejora del zoom

---
+++ ui.apps/src/main/content/jcr_root/apps/itv/components/supertext/clientlib/js/supertext.js
@@ -0,0 +1,10 @@
+
+(function (document) {
+  "use strict";
+      document.querySelector(".cmp-supertexto").addEventListener("click", ()=>{
+        document.querySelector(".cmp-supertexto").classList.toggle("cmp-supertexto--colorRojo");
+      })
+
+  
+})(document);
+
